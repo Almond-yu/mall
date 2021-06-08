c72789b086434b0b9024fb282cf5c06ce0d5237f
Almod-yu <2712430447@qq.com>
2021-06-08 19:35:08 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <MyTabbar/>
+   
    
   </div>
 </template>
   
+<script>
+import MyTabbar from "@/components/content/MyTabbar"
+  export default{
+    name:'app',
+    components:{
+      MyTabbar
+    }
+  }
+</script>
+  
 <style>
+@import "./assets/css/base.css";
+
 </style>
 
 
---
+++ src/components/content/MyTabbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <Tabbar>
+      <TabbarItem path="/home" activeColor="red">
+      <img slot="item-icon" src="@/assets/img/home.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/home-active.png" alt="">
+      <div slot="item-text">首页</div>
+      </TabbarItem>
+
+      <TabbarItem path="/category" activeColor="red">
+      <img slot="item-icon" src="@/assets/img/class.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/class-active.png" alt="">
+      <div slot="item-text">分类</div>
+      </TabbarItem>
+
+      <TabbarItem path="/cart" activeColor="red">
+      <img slot="item-icon" src="@/assets/img/shopping.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/shopping-active.png" alt="">
+      <div slot="item-text">购物车</div>
+      </TabbarItem>
+
+      <TabbarItem path="/profile" activeColor="red">
+      <img slot="item-icon" src="@/assets/img/my.png" alt="">
+       <img slot="item-icon-active" src="@/assets/img/my-active.png" alt="">
+      <div slot="item-text">我的</div>
+      </TabbarItem>
+   </Tabbar>
+</template>
+
+<script>
+import Tabbar from '@/components/common/Tabbar'
+import TabbarItem from '@/components/common/TabbarItem'
+export default {
+  name:'Mytabbar',
+  components:{
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
